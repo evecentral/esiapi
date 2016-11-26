fa5e3df8de3be173dfd561c82146bc24bbe27c5d
Yann Ramin <atrus@stackworks.net>
2016-11-26 02:33:53 +0000

    Add a cloud datastore source

---
+++ helper/datastore_token.go
@@ -0,0 +1,42 @@
+package helper
+
+import (
+	"context"
+	"flag"
+	"github.com/theatrus/ooauth2"
+	"cloud.google.com/go/datastore"
+)
+
+type cloudDatastoreStore struct {
+	client *datastore.Client
+	prefix string
+}
+
+func DatastoreToken(project, prefix string) ooauth2.Option {
+	return func(o *ooauth2.Options) error {
+		ctx := context.Background()
+
+		client, err := datastore.NewClient(ctx, project)
+		if err != nil {
+			return err
+		}
+
+		t, err := client.NewTransaction(ctx)
+		if err != nil {
+			return fmt.Errorf("datastoredb: could not connect: %v", err)
+
+		}
+
+		if err := t.Rollback(); err != nil {
+			return fmt.Errorf("datastoredb: could not connect: %v", err)
+
+		}
+
+		ts := &cloudDatastoreStore{
+			prefix: prefix,
+			client: client,
+		}
+		o.TokenStore = ts
+		return nil
+	}
+}
